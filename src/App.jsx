d160c78788aa1e3ae4d18da4d68d1d5e8c6e3772
yykim <rladuddbs789@gmail.com>
2025-01-24 09:03:24 +0900

    샘플 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SampleMolecules from './components/molecules/SampleMolecules'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>안녕하세요! 서희령 연습</h1>
-      <p>1조 화이팅!!!</p>
-      <p>test</p>
-      <p>test2</p>
-      <p>test3</p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>안녕하세요! 서희령 연습</h1>
+            <p>1조 화이팅!!!</p>
+            <p>test</p>
+            <p>test2</p>
+            <p>test3</p>
+            <SampleMolecules />
+        </>
+    )
 }
 
 export default App
