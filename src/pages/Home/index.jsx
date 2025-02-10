3ebbd557a5af956ae9b633b9ae7477bd12552907
KimJaeKung <rnrrkeo@gmail.com>
2025-02-10 20:13:06 +0900

    태그추가

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,23 @@
+import Tag from '../../components/atoms/Tag'
 import { confirmModalState } from '../../shared/recoil/confirmModalState'
 import { useRecoilState } from 'recoil'
 
 const Home = () => {
-    const [modalOpen, setModalOpen] = useRecoilState(confirmModalState)
+  const [modalOpen, setModalOpen] = useRecoilState(confirmModalState)
 
-    const handleModal = () => {
-        setModalOpen({ isOpen: true })
-    }
+  const handleModal = () => {
+    setModalOpen({ isOpen: true })
+  }
 
-    return (
-        <div>
-            <button onClick={handleModal}>모달 오픈</button>
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={handleModal}>모달 오픈</button>
+      <Tag type="yellow">판타지</Tag>
+      <Tag type="gray">나와요</Tag>
+      <Tag type="skyblue">판타지</Tag>
+      <Tag type="">내향적인 관찰자</Tag>
+    </div>
+  )
 }
 
 export default Home
