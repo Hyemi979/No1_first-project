d286ba544f57fdbb6171c34fdb7360b4201ee418
Hyemi979 <hyemig734@gmail.com>
2025-02-11 20:50:05 +0900

    전체보기 버튼 추가

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,12 @@
+import Button from '../../components/atoms/Button'
 import { confirmModalState } from '../../shared/recoil/confirmModalState'
 import { useRecoilState } from 'recoil'
+import Viewbutton from '../../components/atoms/Viewbutton';
 
-const Home = () => {
+
+const Home = () => { 
     const [modalOpen, setModalOpen] = useRecoilState(confirmModalState)
-
+ 
     const handleModal = () => {
         setModalOpen({ isOpen: true })
     }
@@ -11,6 +14,10 @@ const Home = () => {
     return (
         <div>
             <button onClick={handleModal}>모달 오픈</button>
+            <Button disabled>다음</Button>
+            <Viewbutton>전체보기</Viewbutton>
+        
+
         </div>
     )
 }
