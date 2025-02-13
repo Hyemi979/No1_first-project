b034eca2bb3161f3cadb82ea55c8499eda32f5ad
yykim <rladuddbs789@gmail.com>
2025-02-13 14:19:38 +0900

    fix: title 버그 수정

--- src/components/atoms/Title/index.jsx
+++ src/components/atoms/Title/index.jsx
@@ -1,49 +1,21 @@
-import styles from './index.module.css'
+import styles from './index.module.css';
 
-const Title = ({ type }) => {
-    if (type === 'Title01') {
-        return (
-            <div className={styles.Title_01_Regular}>
-            <Title>안녕하세요!</Title>
-            </div>
-        )
-    } else if (type === 'Title02') {
-        return (
-            <div className={styles.Title_02_Regular}>
-                <Title>안녕하세요!</Title>
-            </div>
-        )
-    } else if (type === 'SubTitle02') {
-        return (
-            <div className={styles.SubTitle_02_Regular}>
-                <Title>안녕하세요!</Title>
-            </div>
-        )
-    } else if (type === 'Body01') {
-        return (
-            <div className={styles.Body_01_Regular}>
-                <Title>안녕하세요!</Title>
-            </div>
-        )
-    } else if (type === 'Body02') {
-        return (
-            <div className={styles.Body_02_Regular}>
-                <Title>안녕하세요!</Title>
-            </div>
-        )
-    } else if (type === 'Caption01') {
-        return (
-            <div className={styles.Caption_01_Regular}>
-                <Title>안녕하세요!</Title>
-            </div>
-        )
-    } else if (type === 'Caption02') {
-        return (
-            <div className={styles.Caption_02_Regular}>
-                <Title>안녕하세요!</Title>
-            </div>
-        )
-    }
-}
+const Title = ({ children, type = 'Title01' }) => {
+  if (type === 'Title01') {
+    return <h2 className={styles.Title_01_Regular}>{children}</h2>;
+  } else if (type === 'Title02') {
+    return <h2 className={styles.Title_02_Regular}>{children}</h2>;
+  } else if (type === 'SubTitle02') {
+    return <h2 className={styles.SubTitle_02_Regular}>{children}</h2>;
+  } else if (type === 'Body01') {
+    return <h2 className={styles.Body_01_Regular}>{children}</h2>;
+  } else if (type === 'Body02') {
+    return <h2 className={styles.Body_02_Regular}>{children}</h2>;
+  } else if (type === 'Caption01') {
+    return <h2 className={styles.Caption_01_Regular}>{children}</h2>;
+  } else if (type === 'Caption02') {
+    return <h2 className={styles.Caption_02_Regular}>{children}</h2>;
+  }
+};
 
-export default Title
+export default Title;
