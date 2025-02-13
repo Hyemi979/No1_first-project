0555573b8b76b8b960cadc9a4d4758b4ec00d703
yykim <rladuddbs789@gmail.com>
2025-02-13 14:25:16 +0900

    feat: step1번 기본 골조 제작

--- src/components/molecules/OnBoardingStep01/index.jsx
+++ src/components/molecules/OnBoardingStep01/index.jsx
@@ -1,12 +1,18 @@
 import Button from '../../atoms/Button';
 import Title from '../../atoms/Title';
+import AppLogo from '../../../assets/app_logo.svg';
+import styles from './index.module.css';
 
 const OnBoardingStep01 = ({ nextStep }) => {
   return (
-    <div>
-      <Title>지금 꿈꾸고 있는 나의 미래의 모습이 있나요?</Title>
-      <Button onClick={() => nextStep('yes')}>예</Button>
-      <Button onClick={() => nextStep('no')}>아니요</Button>
+    <div className={styles.wrap}>
+      <Title>ChapChap</Title>
+      <img src={AppLogo} />
+      <div className={styles.button_wrap}>
+        <Button onClick={() => nextStep('ok')}>카카오로 로그인</Button>
+        <Button onClick={() => nextStep('ok')}>네이버로 로그인</Button>
+        <Button onClick={() => nextStep('ok')}>애플로 로그인</Button>
+      </div>
     </div>
   );
 };
