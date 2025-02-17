cbbb93ed1037477b97bd5aa5ff56f32badc36889
Hyemi979 <hyemig734@gmail.com>
2025-02-17 16:37:15 +0900

    온보딩09

--- src/components/molecules/OnBoardingStep09/index.jsx
+++ src/components/molecules/OnBoardingStep09/index.jsx
@@ -1,55 +1,27 @@
-import React, { useState } from 'react';
-import FavoriteTopNavbar from '../../components/atoms/FavoriteTopNavbar';
-import ProgressBar from '../../components/atoms/ProgressBar';
-import TextField from '../../components/atoms/TextField';
-import Favorite_selection_button from '../../components/atoms/CheckButton';
-import styles from './index.module.css';
-import favoriteimage from '../../assets/image/favoriteimage.svg';
-import { useNavigate } from 'react-router-dom'; // 페이지 이동을 위한 useNavigate
+import { useRef } from 'react';
+import Input from '../../atoms/Input';
+import Title from '../../atoms/Title';
+import Button from '../../atoms/Button';
+import useNavigationPage from '../../../hooks/useNavigationPage';
 
-const FavoritePage = () => {
-  const [isPress, setIsPress] = useState(false);
-  const navigate = useNavigate(); // useNavigate 훅 사용
+const OnBoardingStep07 = ({ nextStep }) => {
+  const ref = useRef(null);
+  const { routePage } = useNavigationPage();
 
-  const handlePage = () => {
-    // 뒤로가기 버튼 클릭 시 이전 페이지로 이동
-    navigate(-1); // -1은 이전 페이지로 돌아가기
-  };
+  const handleNextStep = () => {
+    const item = ref.current.value;
 
-  const handleNextPage = () => {
-    // 버튼 클릭 시 다음 페이지로 이동
-    navigate('/nextPage'); // /nextPage는 이동할 경로
-  };
-
-  const handleClick = () => {
-    setIsPress(!isPress);
+    nextStep(item);
+    routePage('/', item);
   };
 
   return (
-    <div className={styles.pageWrapper}>
-      {/* FavoriteTopNavbar 컴포넌트 */}
-      <FavoriteTopNavbar onClick={handlePage}>
-        나의 독서 스타일은?
-      </FavoriteTopNavbar>
-
-      {/* 프로그레스 바 */}
-      <ProgressBar progress={60} />
-
-      {/* My Taste 이미지 */}
-      <img src={favoriteimage} alt="My Taste" className={styles.myTaste} onClick={handlePage} />
-
-      {/* 텍스트 필드 */}
-      <TextField placeholder="(최소 10자 이상)" />
-
-      {/* 버튼 2개 */}
-      <div className={styles.buttonsWrapper}>
-        <Favorite_selection_button isPress={isPress} onClick={handleClick}>
-          취향 선택 버튼
-        </Favorite_selection_button>
-        <Favorite_selection_button onClick={handleNextPage}>다음 페이지</Favorite_selection_button>
-      </div>
+    <div>
+      <Title>꿈꾸고 있는 미래의 모습을 적어줘</Title>
+      <Input inputRef={ref} />
+      <Button onClick={handleNextStep}>다음</Button>
     </div>
   );
 };
 
-export default FavoritePage;
+export default OnBoardingStep07;
