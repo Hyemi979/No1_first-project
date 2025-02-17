9c7d5119289b230a2fd541f305eda98fb9eb45d3
yoon ju yeong <onl24997@gmail.com>
2025-02-17 16:18:43 +0900

    챕챕 내에서 사용할 이름을 작성해주세요!

--- src/components/molecules/OnBoardingStep03/index.jsx
+++ src/components/molecules/OnBoardingStep03/index.jsx
@@ -3,41 +3,50 @@ import Input from '../../atoms/Input';
 import Title from '../../atoms/Title';
 import Button from '../../atoms/Button';
 import styles from './index.module.css';
-
+import ProgressBar from '../../atoms/ProgressBar';
 const OnBoardingStep02 = ({ nextStep }) => {
   const [isAccept, setIsAccept] = useState(true);
   const ref = useRef(null);
-
   const handleNextStep = () => {
     const item = ref.current.value;
-
     nextStep(item);
   };
-
   const handleChange = (e) => {
     const value = e.target.value;
     if (value !== '' && value !== undefined) {
       setIsAccept(false);
     }
   };
-
+  const [progress, setProgress] = useState(1);
+  const increaseProgress = () => {
+      if (progress < 7) {
+        setProgress(progress + 1);
+      }
+    }
+    const Input = ({ inputRef, ...rest }) => {
+      return <input ref={inputRef} {...rest} className={styles.wrap2}></input>;
+    };
   return (
-    <div className={styles.wrap}>
-      <Title>
+<div className={styles.wrap}>
+<div className={styles.align_progress}>
+<ProgressBar progress={progress} />
+</div>
+        <div className={styles.text_box}>
         챕챕 내에서
-        <br />
+      <br />
         사용할 이름을 작성해주세요
-      </Title>
+        </div>
       <Input
         inputRef={ref}
         placeholder='본명이나 닉네임을 입력해주세요!'
         onChange={handleChange}
       />
-      <Button disabled={isAccept} onClick={handleNextStep}>
+    <div className={styles.wrapper}>
+    <Button disabled={isAccept} onClick={handleNextStep}>
         다음
       </Button>
     </div>
+    </div>
   );
 };
-
 export default OnBoardingStep02;
