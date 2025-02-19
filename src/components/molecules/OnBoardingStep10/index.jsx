41e29cd3c92f03b7659c9515aba6faa29d67a728
yoon ju yeong <onl24997@gmail.com>
2025-02-19 11:57:34 +0900

    로딩페이지 이미지 사이즈 변경

--- src/components/molecules/OnBoardingStep10/index.jsx
+++ src/components/molecules/OnBoardingStep10/index.jsx
@@ -3,7 +3,7 @@ import loading from '../../../assets/loading.gif'
 const OnBoardingStep10 = ({ ...rest }) => {
     return (
         <div>
-        <img src={loading} alt='chaploading' height='100px' width='200px' /> 
+        <img src={loading} alt='chaploading' height='192px' width='278px' /> 
       </div>
     )
 }
