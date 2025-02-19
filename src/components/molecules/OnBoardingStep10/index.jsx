5cdfa4f0490d67331344adc1a613430dffebb89f
yoon ju yeong <onl24997@gmail.com>
2025-02-19 12:06:15 +0900

    로딩페이지 수정

--- src/components/molecules/OnBoardingStep10/index.jsx
+++ src/components/molecules/OnBoardingStep10/index.jsx
@@ -1,11 +1,9 @@
 import loading from '../../../assets/loading.gif'
-
 const OnBoardingStep10 = ({ ...rest }) => {
     return (
-        <div>
-        <img src={loading} alt='chaploading' height='192px' width='278px' /> 
+        <div className={styles.loading_image}>
+        <img src={loading} alt='chaploading' height='250px' width='278px' />
       </div>
     )
 }
-
 export default OnBoardingStep10
