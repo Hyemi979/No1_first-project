a5a651edebd4d23d238b99f900f7e507835a55f4
yykim <rladuddbs789@gmail.com>
2025-02-21 09:40:54 +0900

    feat: 모달 테스트용 코드 추가

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import TextField from '../../components/atoms/TextField';
 import myTaste from '../../assets/image/main_mytaste.svg';
 import useNavigationPage from '../../hooks/useNavigationPage';
+import useCallModal from '../../hooks/useCallModal';
 import styles from './index.module.css';
 
 const Home = () => {
   const { routePage } = useNavigationPage();
+  const { callModal } = useCallModal();
 
   const handlePage = () => {
     routePage('/preference');
@@ -14,6 +16,7 @@ const Home = () => {
     <div className={styles.wrap}>
       <img src={myTaste} onClick={handlePage} className={styles.my_taste} />
       <button onClick={() => routePage('/shortTerm')}>단기 챕</button>
+      <button onClick={callModal}>모달 오픈</button>
     </div>
   );
 };
