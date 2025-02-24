4f30cb7feb2db633192b3cff95ffff79694b7fdb
yykim <rladuddbs789@gmail.com>
2025-02-24 16:38:02 +0900

    fix: 빌드 에러 수정

--- src/pages/ChatRoom/index.jsx
+++ src/pages/ChatRoom/index.jsx
@@ -1,5 +1,3 @@
-
-
 import Chatting from '../../components/molecules/Chatting';
 import profile_image01 from '/src/assets/image/profile_image01.svg';
 import profile_image02 from '/src/assets/image/profile_image02.svg';
@@ -8,18 +6,10 @@ import profile_image04 from '/src/assets/image/profile_image04.svg';
 import profile_image05 from '/src/assets/image/profile_image05.svg';
 import profile_image06 from '/src/assets/image/profile_image06.svg';
 import seed_badge from '/src/assets/image/seed_badge.svg';
-import tree_badgeY from '/src/assets/image/tree_badgeY.svg';
-import tree_badgeB from '/src/assets/image/tree_badgeB.svg';
 import DebateTopicCard from '../../components/molecules/DebateTopicCard';
-import TopNavBar from '../../components/atoms/TopNavbar';
+import TopNavBar from '../../components/atoms/TopNavBar';
 import styles from './index.module.css';
-import React, { useRef, useEffect, useState } from 'react';
-
-
-
-
-
-
+import { useRef, useEffect, useState } from 'react';
 
 const datas = [
   {
@@ -36,7 +26,8 @@ const datas = [
     profileImg: profile_image02,
     name: '김혜미',
     sex: '여자',
-    contents: '가족들과 따뜻한 밥 한 끼를 먹으며, 그동안 못했던 이야기를 나누고 싶어요.',
+    contents:
+      '가족들과 따뜻한 밥 한 끼를 먹으며, 그동안 못했던 이야기를 나누고 싶어요.',
     emojis: [
       { emoji: '😭', count: 20 },
       { emoji: '🍚', count: 26 },
@@ -57,7 +48,8 @@ const datas = [
     profileImg: profile_image04,
     name: '이주영',
     sex: '여자',
-    contents: '인생의 중요한 가르침을 주셨던 선생님께 감사 인사를 전하고 싶어요.',
+    contents:
+      '인생의 중요한 가르침을 주셨던 선생님께 감사 인사를 전하고 싶어요.',
     emojis: [
       { emoji: '💛', count: 26 },
       { emoji: '😊', count: 14 },
@@ -67,7 +59,8 @@ const datas = [
     profileImg: profile_image05,
     name: '김예림',
     sex: '여자',
-    contents: '너무 빨리 떠나보낸 반려동물을 다시 만나서 더 많은 사랑을 주고 싶어요.',
+    contents:
+      '너무 빨리 떠나보낸 반려동물을 다시 만나서 더 많은 사랑을 주고 싶어요.',
     emojis: [
       { emoji: '😭', count: 26 },
       { emoji: '💙', count: 22 },
@@ -87,7 +80,8 @@ const datas = [
     profileImg: profile_image01,
     name: '김맹구',
     sex: '남자',
-    contents: '아리스토텔레스 선생님을 만나서 인생을 후회 없이 보내는 방법을 배우고 싶어요.',
+    contents:
+      '아리스토텔레스 선생님을 만나서 인생을 후회 없이 보내는 방법을 배우고 싶어요.',
     emojis: [
       { emoji: '👍', count: 26 },
       { emoji: '📜', count: 15 },
@@ -128,18 +122,17 @@ const datas = [
     name: '신짱구',
     sex: '남자',
     contents: '흰둥이와 다시 산책하고 싶어요.',
-    
+
     emojis: [
       { emoji: '😭', count: 26 },
       { emoji: '🐕', count: 22 },
     ],
-    
   },
   {
     profileImg: profile_image06,
     name: '나미리',
     sex: '여자',
-    isMe :true,
+    isMe: true,
     contents: '1등 당첨 로또 번호를 외워서 내 집을 마련하고 싶어요.',
     emojis: [
       { emoji: '💵', count: 28 },
@@ -149,7 +142,6 @@ const datas = [
 ];
 
 const ChatRoom = () => {
-
   const topNavBarRef = useRef(null);
   const debateTopicCardRef = useRef(null);
   const [paddingTop, setPaddingTop] = useState(0);
@@ -171,21 +163,25 @@ const ChatRoom = () => {
 
   return (
     <div>
-    {/* 상단 고정 섹션 */}
-    <div className={styles.topsection}>
-      <TopNavBar ref={topNavBarRef} navtitle="단기챕 채팅방" bell={true} settings={true} />
-      <DebateTopicCard
-      ref={debateTopicCardRef}
-        subtitle="[세상의 마지막 기차역] 토론 주제"
-        title="만약 단 하루, 과거로 돌아갈 수 있다면 누구를 만나고 싶나요?"
-        replyCount="32"
-        onHeightChange={handleHeightChange}
-      />
-    </div>
-
+      {/* 상단 고정 섹션 */}
+      <div className={styles.topsection}>
+        <TopNavBar
+          ref={topNavBarRef}
+          navtitle='단기챕 채팅방'
+          bell={true}
+          settings={true}
+        />
+        <DebateTopicCard
+          ref={debateTopicCardRef}
+          subtitle='[세상의 마지막 기차역] 토론 주제'
+          title='만약 단 하루, 과거로 돌아갈 수 있다면 누구를 만나고 싶나요?'
+          replyCount='32'
+          onHeightChange={handleHeightChange}
+        />
+      </div>
 
       {/* 채팅 내용 섹션 */}
-      <div className={styles.container} >
+      <div className={styles.container}>
         {datas.map((data, index) => (
           <Chatting
             key={index}
